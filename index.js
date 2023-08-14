1fb45d0c819075650f4c07bbe35648086a567484
michaelalves <michael.alves@ctw.bmwgroup.com>
2023-08-14 10:15:04 +0100

    feat: add new aspect ratio and selector

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const dropzone = document.querySelector('.dropzone')
 const image1 = document.getElementById('image1')
 const image2 = document.getElementById('image2')
 const opacitySlider = document.getElementById('opacitySlider')
+const aspectRatioInput = document.querySelectorAll('input[name="aspect-ratio"]');
+const imageWrapper = document.getElementById('image-wrapper')
 
 const DRAG_OVER_EVENT = 'dragover'
 const DRAG_LEAVE_EVENT = 'dragleave'
 const DROP_EVENT = 'drop'
 const INPUT_EVENT = 'input'
+
+const aspectRadioClassMap = {
+    '2:1': 'medium_aspect_ratio',
+    '3:1': 'large_aspect_ratio'
+}
 
 const handleDragEvent = (event, type) => {
     event.preventDefault()
@@ -39,6 +46,18 @@ dropzone.addEventListener(DROP_EVENT, e => {
     const files = e.dataTransfer.files
 
     readFiles(files)
+})
+
+aspectRatioInput.forEach((input) => {
+    input.addEventListener('click', ({ target }) => {
+        if (target.value == "2:1") {
+            imageWrapper.classList.remove(aspectRadioClassMap["3:1"])
+        } else {
+            imageWrapper.classList.remove(aspectRadioClassMap["2:1"])
+        }
+
+        imageWrapper.classList.add(aspectRadioClassMap[target.value])
+    })
 })
 
 opacitySlider.addEventListener(INPUT_EVENT, () => {
